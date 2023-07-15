1931754f3be790a3abb96aeab9723d02f00b9515
Astero <adenv3@gmail.com>
2023-07-16 01:54:58 +0800

    Update AbstractCommand.java

--- src/main/java/bot/commands/AbstractCommand.java
+++ src/main/java/bot/commands/AbstractCommand.java
@@ -6,5 +6,5 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 public abstract class AbstractCommand {
 
     protected boolean allowPrefixReplies = false; // whether the bot will do something when the prefix is said by the user.
-    public void onCommandReceived(Update update, TelegramLongPollingBot telegramBot) {}
+    public abstract void onCommandReceived(Update update, TelegramLongPollingBot telegramBot);
 }
